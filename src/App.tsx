d31f3cedef806d4fab144eed080a3332a8ac24db
Evelyn Jiang <115804711+eve1ynjiang@users.noreply.github.com>
2025-06-12 16:34:45 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content :D") });
+    client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
   function deleteTodo(id: string) {
